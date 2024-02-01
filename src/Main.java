1251648d97217725fb8af6f3e648bb9de8c20774
Gladysrono <Gladyschepkoech408@gmail.com>
2024-02-01 04:55:57 -0800

    project from intelli j

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        String name="gladys";
+
+        System.out.println(name.toUpperCase());
+        String glad="gladys";
+        System.out.println(name.contains(glad));
+        System.out.println(name.equals(glad));
+
+    }
+}
